92ee43ad67a4decb4786b7f63d13e165d0071700
Rachel Barkanic <rachel.barkanic@gmail.com>
2022-08-25 19:22:29 -0700

    problem 4

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -46,7 +46,7 @@ let evensArr = []
   Use a for-loop to iterate through each number in the 'nums' array, if the number is even, append it to the 'evensArr'.
 */
 
-for (let i = 0; i < nums.length[i]; i++) {
+for (let i = 0; i < nums.length; i++) {
   if (nums[i] % 2 === 0) {
     evensArr.push(nums[i])
   }
